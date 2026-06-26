1e74b1e53feccdc892bfd02476b327b53c0471f3
MOHAMMED-BOUDIA <mohamedboudia516@gmail.com>
2026-06-26 22:14:06 +0100

    debug: test authRoutes require

--- server/server.js
+++ server/server.js
@@ -3,7 +3,15 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
+
 app.get('/', (req, res) => res.json({ status: 'ok' }));
+
+try {
+  const authRoutes = require('./routes/authRoutes');
+  app.use('/api/auth', authRoutes);
+} catch (e) {
+  app.get('/api/auth/login', (req, res) => res.status(500).json({ error: e.message }));
+}
 
 app.use((err, req, res, next) => {
   res.status(500).json({ message: 'Server error', error: err.message });
